fc30980ac4e1ad1fed5cde4e10e9c1dc798cf4d0
Lavanya <142294510+Lavanya-rao05@users.noreply.github.com>
2024-09-28 14:48:42 +0530

    Inital commit

---
+++ musicLibary.py
@@ -0,0 +1,12 @@
+music = {
+    "anti-hero": "https://www.youtube.com/watch?v=b1kbLwvqugk",  # Taylor Swift - Anti-Hero
+    "flowers": "https://www.youtube.com/watch?v=G7KNmW9a75Y",    # Miley Cyrus - Flowers
+    "unholy": "https://www.youtube.com/watch?v=PYDs_P59q8U",      # Sam Smith, Kim Petras - Unholy
+    "kill_bill": "https://www.youtube.com/watch?v=OxlJLz9M8cs",   # SZA - Kill Bill
+    "as_it_was": "https://www.youtube.com/watch?v=H5v3kku4y6Q",   # Harry Styles - As It Was
+    "calm_down": "https://www.youtube.com/watch?v=6I2FpUV7XjI",   # Rema, Selena Gomez - Calm Down
+    "creepin": "https://www.youtube.com/watch?v=ZVqfp1glTZk",     # Metro Boomin, The Weeknd - Creepin'
+    "rich_flex": "https://www.youtube.com/watch?v=l1ZyAV5aMZw",   # Drake, 21 Savage - Rich Flex
+    "bad_habit": "https://www.youtube.com/watch?v=9AjkUyX0rVw",   # Steve Lacy - Bad Habit
+    "under_the_influence": "https://www.youtube.com/watch?v=IhxbvVmADLk" 
+}
